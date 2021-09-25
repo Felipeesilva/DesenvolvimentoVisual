cebf00665a40c1ed31b1288043650c7b1b84f632
Felipe Silva <lipe1398@gmail.com>
2021-09-25 17:15:01 -0300

    atualização component

--- FRONT/src/app/app.component.ts
+++ FRONT/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { ProdutoService } from './services/produto.service';
+import { Component, OnInit } from '@angular/core';
+import { Produto } from './models/produto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: [],
 })
-export class AppComponent {
-  title = 'FRONT';
+
+export class AppComponent implements OnInit{
+
+    produtos: Produto[] = [];
+
+    constructor(private service: ProdutoService){}
+
+    ngOnInit(): void {
+        this.service.list().subscribe((produtos) => {
+            this.produtos = produtos;
+
+            console.log(produtos);
+        });
+
+    }
 }
